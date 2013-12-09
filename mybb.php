e68616080cb822c1d1a31cbd21a2ef5d4ea36aff
Eric Hocking <erich@xaos-ia.com>
2013-12-09 08:53:30 -0500

    Update mybb.php
    added whmcs admin area sidebar

--- mybb.php
+++ mybb.php
@@ -36,13 +36,48 @@ function mybb_deactivate() {
 
 function mybb_upgrade($vars) {
   
+  //get the current version
+  $version = $vars['version'];
+  
 }
 
 function mybb_output($vars) {
   
+  //module link
+  $modulelink = $vars['modulelink'];
+  
+  //version
+  $version = $vars['version'];
+  
+  //mybb settings
+  
 }
 
 function mybb_sidebar($vars) {
+  
+  //module link
+  $modulelink = $vars['modulelink'];
+  
+  //version
+  $version = $vars['version'];
+  
+  //sidebar content
+  $sidebar = "<span class='header'>
+  <img src='images/icons/addonmodules.png' class='absmiddle' width='16' height='16'>
+  MyBB Bridge
+  </span>
+  <ul class='menu'>
+    <li><a href='$modulelink/admin/index.php?module=config'>Configuration</a></li>
+    <li><a href='$modulelink/admin/index.php?module=forum'>Forums & Posts</a></li>
+    <li><a href='$modulelink/admin/index.php?module=user'>Users & Groups</a></li>
+    <li><a href='$modulelink/admin/index.php?module=style'>Templates & Style</a></li>
+    <li><a href='$modulelink/admin/index.php?module=tools'>Tools & Maintenance</a></li>
+    <li><a href='#'>Version: $version</a></li>
+  </ul>
+  ";
+  
+  //return the sidebar
+  return $sidebar;
   
 }
 
